237b64d7f9ab28d3b049d7815678195213ea4ea7
erlingrj <erling.jellum@gmail.com>
2024-05-21 23:23:23 +0200

    Fix

--- test/event_queue_test.c
+++ test/event_queue_test.c
@@ -7,7 +7,8 @@ void test_insert(void) {
   EventQueue q;
   EventQueue_ctor(&q);
   TEST_ASSERT_TRUE(q.empty(&q));
-  TEST_ASSERT_TRUE(lf_tag_compare(q.next_tag(&q), FOREVER_TAG));
+  tag_t t = FOREVER_TAG;
+  TEST_ASSERT_EQUAL(lf_tag_compare(q.next_tag(&q), t), 0);
   Event e = {.tag = {.time = 100}};
   q.insert(&q, e);
   TEST_ASSERT_EQUAL(lf_tag_compare(q.next_tag(&q), e.tag), 0);
