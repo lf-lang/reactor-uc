52903c9e1ef955ff585e2aef80e6b2539c55547e
erling <erling.jellum@gmail.com>
2024-05-21 14:27:26 -0700

    Add test (#3)
    Fix CI printing coverage info

--- test/event_queue_test.c
+++ test/event_queue_test.c
@@ -7,6 +7,8 @@ void test_insert(void) {
   EventQueue q;
   EventQueue_ctor(&q);
   TEST_ASSERT_TRUE(q.empty(&q));
+  tag_t t = FOREVER_TAG;
+  TEST_ASSERT_EQUAL(lf_tag_compare(q.next_tag(&q), t), 0);
   Event e = {.tag = {.time = 100}};
   q.insert(&q, e);
   TEST_ASSERT_EQUAL(lf_tag_compare(q.next_tag(&q), e.tag), 0);
