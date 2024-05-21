5d469b299e34b3a8a6149804ce590cb763c82978
erlingrj <erling.jellum@gmail.com>
2024-05-21 23:20:48 +0200

    Add test

--- test/event_queue_test.c
+++ test/event_queue_test.c
@@ -7,6 +7,7 @@ void test_insert(void) {
   EventQueue q;
   EventQueue_ctor(&q);
   TEST_ASSERT_TRUE(q.empty(&q));
+  TEST_ASSERT_TRUE(lf_tag_compare(q.next_tag(&q), FOREVER_TAG));
   Event e = {.tag = {.time = 100}};
   q.insert(&q, e);
   TEST_ASSERT_EQUAL(lf_tag_compare(q.next_tag(&q), e.tag), 0);
