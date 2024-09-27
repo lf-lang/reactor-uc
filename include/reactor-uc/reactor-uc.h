d341e3364e0f8aa12cfaa87fdd50e5da37a542c7
Martin Schoeberl <martin@jopdesign.com>
2024-09-27 13:36:08 -0700

    fix formatting

--- include/reactor-uc/reactor-uc.h
+++ include/reactor-uc/reactor-uc.h
@@ -6,12 +6,12 @@
 #include "reactor-uc/connection.h"
 #include "reactor-uc/environment.h"
 #include "reactor-uc/macros.h"
+#include "reactor-uc/platform.h"
 #include "reactor-uc/port.h"
 #include "reactor-uc/reaction.h"
 #include "reactor-uc/reactor.h"
 #include "reactor-uc/tag.h"
 #include "reactor-uc/timer.h"
-#include "reactor-uc/platform.h"
 #include "reactor-uc/trigger.h"
 #include <assert.h>
 #include <stdio.h>
