261421e0f1fc23d6ae5801e3796c9297baaf3f5a
tanneberger <github@tanneberger.me>
2024-10-04 21:51:23 +0200

    factoring out encoding and decoding of protobuf

--- examples/posix/testing_nanopb.c
+++ examples/posix/testing_nanopb.c
@@ -5,6 +5,7 @@
 #include <nanopb/pb_decode.h>
 
 #include "../../../reactor-uc/include/reactor-uc/generated/message.pb.h"
+#include "../../../reactor-uc/include/reactor-uc/encoding.h"
 int main() {
 
   PortMessage original_message;
@@ -17,29 +18,20 @@ int main() {
   const char* text = "Hello World1234";
   memcpy(original_message.message, text, sizeof("Hello World1234"));
 
-  // turing write buffer into pb_ostream buffer
-  pb_ostream_t stream_out = pb_ostream_from_buffer(buffer, 1024);
-
-  // serializing protobuf into buffer
-  int status = pb_encode(&stream_out, PortMessage_fields, &original_message);
-
-  if (status < 0) {
+  message = buffer;
+  message_size = encode_protobuf(&original_message, buffer, 1024);
+  if (message_size < 0) {
     printf("encoding failed!\n");
     exit(1);
   }
 
-  message_size = stream_out.bytes_written;
-  message = buffer;
+  int remaining_bytes = decode_protobuf(&deserialized_message, message, message_size);
 
-  pb_istream_t stream_in = pb_istream_from_buffer(message, message_size);
-
-  if(!pb_decode(&stream_in, PortMessage_fields, &deserialized_message)) {
-    printf("decoding failed: %s\n", stream_in.errmsg);
+  if (remaining_bytes < 0) {
+    printf("decoding failed!\n");
     exit(1);
   }
 
   printf("o: %i d: %i\n", original_message.connection_number, deserialized_message.connection_number);
   printf("o: %s d: %s\n", original_message.message, deserialized_message.message);
-
-
 }
---
+++ include/reactor-uc/encoding.h
@@ -0,0 +1,33 @@
+#ifndef REACTOR_UC_ENCODING_H
+#define REACTOR_UC_ENCODING_H
+
+#include "generated/message.pb.h"
+#include <nanopb/pb_encode.h>
+#include <nanopb/pb_decode.h>
+
+int encode_protobuf(const PortMessage* message, unsigned char* buffer, size_t buffer_size) {
+  // turing write buffer into pb_ostream buffer
+  pb_ostream_t stream_out = pb_ostream_from_buffer(buffer, buffer_size);
+
+  // serializing protobuf into buffer
+  if (!pb_encode(&stream_out, PortMessage_fields, message)) {
+    printf("protobuf encoding error %s\n", stream_out.errmsg);
+    return -1;
+  }
+
+  return (int)stream_out.bytes_written;
+}
+
+int decode_protobuf(PortMessage* message, const char* buffer, size_t buffer_size) {
+  pb_istream_t stream_in = pb_istream_from_buffer(buffer, buffer_size);
+
+
+  if(!pb_decode(&stream_in, PortMessage_fields, message)) {
+    printf("protobuf decoding error %s\n", stream_in.errmsg);
+    return -1;
+  }
+
+  return (int)stream_in.bytes_left;
+}
+
+#endif // REACTOR_UC_ENCODING_H
