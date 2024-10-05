bc4725c5fc15ea3b84a675fd799f93f050ae77c1
erlingrj <erling.jellum@gmail.com>
2024-10-04 23:12:56 -0700

    Getting it to compile on Zephyr and CI

--- src/platform.c
+++ src/platform.c
@@ -2,9 +2,9 @@
 #include "platform/posix/posix.c"
 #include "platform/posix/tcp_ip_bundle.c"
 #elif defined(PLATFORM_RIOT)
-#include "platform/riot.c"
+#include "platform/riot/riot.c"
 #elif defined(PLATFORM_ZEPHYR)
-#include "platform/zephyr.c"
+#include "platform/zephyr/zephyr.c"
 #else
 #error "NO PLATFORM SPECIFIED"
 #endif
