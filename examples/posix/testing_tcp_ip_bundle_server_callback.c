b188b40b10bf8cba797a4f64932846d577b40f36
Tassilo Tanneberger <github@tanneberger.me>
2024-10-08 02:34:08 +0200

    adding callback mechanism to tcp-ip-bundle (#45)

---
+++ examples/posix/testing_tcp_ip_bundle_server_callback.c
@@ -0,0 +1,38 @@
+#include "reactor-uc/reactor-uc.h"
+#include "reactor-uc/platform/posix/tcp_ip_bundle.h"
+#include <sys/socket.h>
+#include <unistd.h>
+
+void callback_handler(FederatedConnection* self, PortMessage* msg, TcpIpBundle* bundle) {
+  printf("Received message with connection number %i and content %s\n", msg->connection_number, msg->message);
+
+  bundle->send(bundle, msg);
+}
+
+int main() {
+  TcpIpBundle bundle;
+
+  const char* host = "127.0.0.1";
+  unsigned short port = 8900; // NOLINT
+
+  // creating a server that listens on loopback device on port 8900
+  TcpIpBundle_ctor(&bundle, host, port, AF_INET);
+
+  // binding to that address
+  bundle.bind(&bundle);
+
+  // change the bundle to non-blocking
+  bundle.change_block_state(&bundle, false);
+
+  // accept one connection
+  bool new_connection;
+  do {
+     new_connection = bundle.accept(&bundle);
+  } while (!new_connection);
+
+  bundle.register_callback(&bundle, callback_handler, NULL);
+
+  sleep(100);
+
+  bundle.close(&bundle);
+}
--- include/reactor-uc/platform/posix/tcp_ip_bundle.h
+++ include/reactor-uc/platform/posix/tcp_ip_bundle.h
@@ -1,8 +1,10 @@
-#include "reactor-uc/error.h"
 #include <nanopb/pb.h>
+#include <pthread.h>
 #include <sys/select.h>
 
 #include "proto/message.pb.h"
+#include "reactor-uc/connection.h"
+#include "reactor-uc/error.h"
 
 #define TCP_BUNDLE_BUFFERSIZE 1024
 
@@ -24,15 +26,27 @@ struct TcpIpBundle {
   fd_set set;
   bool server;
   bool blocking;
+  bool terminate;
+
+  // required for callbacks
+  pthread_t receive_thread;
+  FederatedConnection *federated_connection;
+  void (*receive_callback)(FederatedConnection *connection, PortMessage *message, TcpIpBundle *bundle);
 
   lf_ret_t (*bind)(TcpIpBundle *self);
   lf_ret_t (*connect)(TcpIpBundle *self);
   bool (*accept)(TcpIpBundle *self);
   void (*close)(TcpIpBundle *self);
   void (*change_block_state)(TcpIpBundle *self, bool blocking);
+  void (*register_callback)(TcpIpBundle *self,
+                            void (*receive_callback)(FederatedConnection *connection, PortMessage *message,
+                                                     TcpIpBundle *bundle),
+                            FederatedConnection *connection);
 
   lf_ret_t (*send)(TcpIpBundle *self, PortMessage *message);
   PortMessage *(*receive)();
 };
 
 void TcpIpBundle_ctor(TcpIpBundle *self, const char *host, unsigned short port, int protocol_family);
+
+void TcpIpBundle_free(TcpIpBundle *self);
