fe041d8855057258ff8a2ce5ce60651944a91768
erlingrj <erling.jellum@gmail.com>
2024-10-17 21:26:51 -0700

    Add flag for posix-tcp

--- src/platform.c
+++ src/platform.c
@@ -1,11 +1,8 @@
 #if defined(PLATFORM_POSIX)
 #include "platform/posix/posix.c"
-#include "platform/posix/tcp_ip_channel.c"
 #elif defined(PLATFORM_RIOT)
-#include "platform/posix/tcp_ip_channel.c"
 #include "platform/riot/riot.c"
 #elif defined(PLATFORM_ZEPHYR)
-#include "platform/posix/tcp_ip_channel.c"
 #include "platform/zephyr/zephyr.c"
 #elif defined(PLATFORM_PICO)
 #include "platform/pico/pico.c"
