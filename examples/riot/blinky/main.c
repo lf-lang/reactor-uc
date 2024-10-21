3c015020a138695ee1117c7ce0151ea8c1efd0ad
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-10-21 14:31:02 +0000

    fixup! Add RIOT examples
    Migrate blinky example to macros

--- examples/riot/blinky/main.c
+++ examples/riot/blinky/main.c
@@ -1,51 +1,37 @@
 #include "board.h"
 #include "reactor-uc/reactor-uc.h"
+#include <inttypes.h>
+
+DEFINE_TIMER(MyTimer, 1, MSEC(0), MSEC(100))
+DEFINE_REACTION(MyReactor, 0, 0)
 
 typedef struct {
-  Timer super;
-  Reaction *effects[1];
-} MyTimer;
-
-typedef struct {
-  Reaction super;
-} MyReaction;
-
-struct MyReactor {
   Reactor super;
-  MyReaction my_reaction;
+  MyReactor_0 my_reaction;
   MyTimer timer;
   Reaction *_reactions[1];
   Trigger *_triggers[1];
-};
+} MyReactor;
 
-void timer_handler(Reaction *_self) {
+REACTION_BODY(MyReactor, 0, {
   LED0_TOGGLE;
-  Environment *env = _self->parent->env;
-  printf("Hello World @ %lld\n", env->get_elapsed_physical_time(env));
-}
+  printf("Hello World @ %" PRId64 "\n", env->get_elapsed_physical_time(env));
+})
 
-void MyReaction_ctor(MyReaction *self, Reactor *parent) {
-  Reaction_ctor(&self->super, parent, timer_handler, NULL, 0, 0);
-}
-
-void MyReactor_ctor(struct MyReactor *self, Environment *env) {
+void MyReactor_ctor(MyReactor *self, Environment *env) {
   self->_reactions[0] = (Reaction *)&self->my_reaction;
   self->_triggers[0] = (Trigger *)&self->timer;
-  Reactor_ctor(&self->super, "MyReactor", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
-  MyReaction_ctor(&self->my_reaction, &self->super);
-  Timer_ctor(&self->timer.super, &self->super, MSEC(0), MSEC(100), self->timer.effects, 1);
 
-  TRIGGER_REGISTER_EFFECT(&self->timer.super, &self->my_reaction.super);
+  Reactor_ctor(&self->super, "MyReactor", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
+  MyReactor_0_ctor(&self->my_reaction, &self->super);
+  MyTimer_ctor(&self->timer, &self->super);
+
+  TIMER_REGISTER_EFFECT(self->timer, self->my_reaction);
 }
 
-struct MyReactor my_reactor;
-Environment env;
+ENTRY_POINT(MyReactor)
 
 int main(void) {
-  Environment_ctor(&env, (Reactor *)&my_reactor);
-  // env.set_stop_time(&env, SEC(1));
-  MyReactor_ctor(&my_reactor, &env);
-  env.assemble(&env);
-  env.start(&env);
+  lf_start();
   return 0;
 }
