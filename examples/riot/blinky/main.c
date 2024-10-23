2ce4aa8a2be64aeea97e06c327d44d1a2693fc98
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-10-23 09:26:01 +0000

    Make RIOT examples compatible with new macros

--- examples/riot/blinky/main.c
+++ examples/riot/blinky/main.c
@@ -2,33 +2,42 @@
 #include "reactor-uc/reactor-uc.h"
 #include <inttypes.h>
 
-DEFINE_TIMER(MyTimer, 1, MSEC(0), MSEC(100))
-DEFINE_REACTION(MyReactor, 0, 0)
+/* Structs */
+DEFINE_TIMER_STRUCT(MyTimer, 1)
+DEFINE_REACTION_STRUCT(MyReactor, 0, 0)
 
 typedef struct {
   Reactor super;
-  MyReactor_0 my_reaction;
+  MyReactor_Reaction0 my_reaction;
   MyTimer timer;
   Reaction *_reactions[1];
   Trigger *_triggers[1];
 } MyReactor;
 
-REACTION_BODY(MyReactor, 0, {
+/* Reaction Bodies */
+DEFINE_REACTION_BODY(MyReactor, 0) {
+  MyReactor *self = (MyReactor *)_self->parent;
+  Environment *env = self->super.env;
   LED0_TOGGLE;
   printf("Hello World @ %" PRId64 "\n", env->get_elapsed_physical_time(env));
-})
+}
+
+/* Constructors */
+DEFINE_TIMER_CTOR_FIXED(MyTimer, 1, MSEC(0), MSEC(100))
+DEFINE_REACTION_CTOR(MyReactor, 0)
 
 void MyReactor_ctor(MyReactor *self, Environment *env) {
   self->_reactions[0] = (Reaction *)&self->my_reaction;
   self->_triggers[0] = (Trigger *)&self->timer;
 
   Reactor_ctor(&self->super, "MyReactor", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
-  MyReactor_0_ctor(&self->my_reaction, &self->super);
+  MyReactor_Reaction0_ctor(&self->my_reaction, &self->super);
   MyTimer_ctor(&self->timer, &self->super);
 
   TIMER_REGISTER_EFFECT(self->timer, self->my_reaction);
 }
 
+/* Application */
 ENTRY_POINT(MyReactor, FOREVER, true)
 
 int main(void) {
--- examples/riot/hello/main.c
+++ examples/riot/hello/main.c
@@ -1,28 +1,38 @@
 #include "reactor-uc/reactor-uc.h"
 #include <inttypes.h>
 
-DEFINE_TIMER(MyTimer, 1, 0, MSEC(100))
-DEFINE_REACTION(MyReactor, 0, 0)
+/* Structs */
+DEFINE_TIMER_STRUCT(MyTimer, 1)
+DEFINE_REACTION_STRUCT(MyReactor, 0, 0)
 
 typedef struct {
   Reactor super;
-  MyReactor_0 my_reaction;
+  MyReactor_Reaction0 my_reaction;
   MyTimer timer;
   Reaction *_reactions[1];
   Trigger *_triggers[1];
 } MyReactor;
 
-REACTION_BODY(MyReactor, 0, { printf("Hello World @ %lld\n", env->get_elapsed_logical_time(env)); })
+/* Reaction Bodies */
+DEFINE_REACTION_BODY(MyReactor, 0) {
+  Environment *env = _self->parent->env;
+  printf("Hello World @ %" PRIi64 "\n", env->get_elapsed_logical_time(env));
+}
+
+/* Constructors */
+DEFINE_TIMER_CTOR_FIXED(MyTimer, 1, 0, MSEC(100))
+DEFINE_REACTION_CTOR(MyReactor, 0)
 
 void MyReactor_ctor(MyReactor *self, Environment *env) {
   self->_reactions[0] = (Reaction *)&self->my_reaction;
   self->_triggers[0] = (Trigger *)&self->timer;
   Reactor_ctor(&self->super, "MyReactor", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
-  MyReactor_0_ctor(&self->my_reaction, &self->super);
+  MyReactor_Reaction0_ctor(&self->my_reaction, &self->super);
   Timer_ctor(&self->timer.super, &self->super, MSEC(0), MSEC(100), self->timer.effects, 1);
   TIMER_REGISTER_EFFECT(self->timer, self->my_reaction);
 }
 
+/* Application */
 ENTRY_POINT(MyReactor, FOREVER, true)
 
 int main() {
