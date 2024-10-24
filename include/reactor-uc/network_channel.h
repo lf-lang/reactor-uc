361a0e473753228021cc70ea88305371ba7da4c6
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-10-24 14:19:50 +0000

    Fix close_connection interface

--- include/reactor-uc/network_channel.h
+++ include/reactor-uc/network_channel.h
@@ -16,7 +16,7 @@ struct NetworkChannel {
   size_t dest_channel_id; // So that we can "address" one of several NetworkChannel's at the other end.
   lf_ret_t (*open_connection)(NetworkChannel *self);
   lf_ret_t (*try_connect)(NetworkChannel *self);
-  lf_ret_t (*close_connection)(NetworkChannel *self);
+  void (*close_connection)(NetworkChannel *self);
   lf_ret_t (*send_blocking)(NetworkChannel *self, TaggedMessage *message);
   void (*register_receive_callback)(NetworkChannel *self,
                                     void (*receive_callback)(FederatedConnectionBundle *conn, TaggedMessage *message),
