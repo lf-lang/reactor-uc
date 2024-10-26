82e72f6ae887311f5b353075ad1173d30f2ae8b9
erling <erling.jellum@gmail.com>
2024-10-25 17:19:57 -0700

    Remove the config.h.in and do things through compile defs (#103)
    * Remove the config.h.in and do things through compile defs
    
    * Improve CMake a little
    
    * Rename NETWORK_POSIX_TCP -> NETWORK_CHANNEL_TCP_POSIX

--- src/network_channel.c
+++ src/network_channel.c
@@ -1,3 +1,23 @@
-#ifdef NETWORK_POSIX_TCP
+#if defined(PLATFORM_POSIX)
+#ifdef NETWORK_CHANNEL_TCP_POSIX
 #include "platform/posix/tcp_ip_channel.c"
+#endif
+
+#elif defined(PLATFORM_ZEPHYR)
+#ifdef NETWORK_CHANNEL_TCP_POSIX
+#include "platform/posix/tcp_ip_channel.c"
+#endif
+
+#elif defined(PLATFORM_RIOT)
+#ifdef NETWORK_CHANNEL_TCP_POSIX
+#include "platform/posix/tcp_ip_channel.c"
+#endif
+
+#elif defined(PLATFORM_PICO)
+#ifdef NETWORK_CHANNEL_TCP_POSIX
+#error "NETWORK_POSIC_TCP not supported on PICO"
+#endif
+
+#else
+#error "Platform not supported"
 #endif
