159473b2347a8c4a19d7c31c56b71e374cc144eb
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-11-08 14:13:37 +0000

    WIP

---
+++ examples/riot/coap_channel/coap_channel.h
@@ -0,0 +1,38 @@
+#ifndef REACTOR_UC_COAP_CHANNEL_H
+#define REACTOR_UC_COAP_CHANNEL_H
+#include "reactor-uc/network_channel.h"
+
+// #define COAP_CHANNEL_BUFFERSIZE 1024
+// #define COAP_CHANNEL_NUM_RETRIES 255;
+// #define COAP_CHANNEL_RECV_THREAD_STACK_SIZE 2048
+// #define COAP_CHANNEL_RECV_THREAD_STACK_GUARD_SIZE 128
+
+typedef enum {
+  SERVER_COAP_CHANNEL,
+  CLIENT_COAP_CHANNEL,
+} CoapChannelType;
+
+typedef struct CoapChannel CoapChannel;
+typedef struct FederatedConnectionBundle FederatedConnectionBundle;
+
+struct CoapChannel {
+  NetworkChannel super;
+
+  const char *local_host;
+  unsigned short local_port;
+  const char *remote_host;
+  unsigned short remote_port;
+
+  bool connected_to_server;
+  bool connected_to_client;
+
+  sock_udp_ep_t remote;
+
+  FederatedConnectionBundle *federated_connection;
+  void (*receive_callback)(FederatedConnectionBundle *conn, const FederateMessage *message);
+};
+
+void CoapChannel_ctor(CoapChannel *self, const char *local_host, unsigned short local_port, const char *remote_host,
+                      unsigned short remote_port);
+
+#endif
