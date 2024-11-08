a810b051995ed2849b22e3bd4df54db09f0f5707
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-11-08 14:13:37 +0000

    WIP

--- examples/riot/coap_channel/coap_channel.c
+++ examples/riot/coap_channel/coap_channel.c
@@ -1,20 +1,81 @@
-#include "reactor-uc/platform/riot/coap_channel.h"
+#include "coap_channel.h"
 #include "reactor-uc/logging.h"
+#include "reactor-uc/environment.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "net/gcoap.h"
+#include "net/sock/udp.h"
+#include "net/sock/util.h"
+#include "od.h"
+#include "uri_parser.h"
+
+static CoapChannel *_get_coap_channel_by_dest_channel_id(uint32_t dest_channel_id) {
+  // TODO: Where to get env from?
+  Environment *env = NULL;
+
+  CoapChannel *channel;
+  for (size_t i = 0; i < env->net_bundles_size; i++) {
+    channel = env->net_bundles[i]->net_channel;
+    if (channel->super.dest_channel_id == dest_channel_id) {
+      return channel;
+    }
+  }
+
+  return NULL;
+}
+
+static int _uristr2remote(const char *uri, sock_udp_ep_t *remote, const char **path, char *buf, size_t buf_len) {
+  if (strlen(uri) >= buf_len) {
+    DEBUG_PUTS("URI too long");
+    return 1;
+  }
+  uri_parser_result_t urip;
+  if (uri_parser_process(&urip, uri, strlen(uri))) {
+    DEBUG("'%s' is not a valid URI\n", uri);
+    return 1;
+  }
+  memcpy(buf, urip.host, urip.host_len);
+  buf[urip.host_len] = '\0';
+  if (urip.port_str_len) {
+    strcat(buf, ":");
+    strncat(buf, urip.port_str, urip.port_str_len);
+    buf[urip.host_len + 1 + urip.port_str_len] = '\0';
+  }
+  if (sock_udp_name2ep(remote, buf) != 0) {
+    DEBUG("Could not resolve address '%s'\n", buf);
+    return -1;
+  }
+  if (remote->port == 0) {
+    remote->port = !strncmp("coaps", urip.scheme, 5) ? CONFIG_GCOAPS_PORT : CONFIG_GCOAP_PORT;
+  }
+  if (path) {
+    *path = urip.path;
+  }
+  strcpy(buf, uri);
+  return 0;
+}
 
 static ssize_t _connect_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx) {
   (void)ctx;
+
+  CoapChannel *self = _get_coap_channel_by_dest_channel_id();
 
   gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
   coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
   size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
 
-  if (self.connected_to_client) {
+  if (self->connected_to_client) {
     // This server is already connected to a client
     return false;
   }
 
   // Set as connected
-  self.connected_to_client = true;
+  self->connected_to_client = true;
 
   // Connect successful
   return true;
@@ -23,23 +84,25 @@ static ssize_t _connect_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_
 static ssize_t _disconnect_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx) {
   (void)ctx;
 
+  CoapChannel *self = _get_coap_channel_by_dest_channel_id();
+
   gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
   coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
   size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
 
-  if (!self.connected_to_client) {
+  if (!self->connected_to_client) {
     // This server is not connected to any client
     return false;
   }
 
   // Set as disconnect
-  self.connected_to_client = false;
+  self->connected_to_client = false;
 
   // Disconnect successful
   return true;
 }
 
-static ssize_t _send_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx) {
+static ssize_t _message_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx) {
   (void)ctx;
 
   gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
@@ -54,21 +117,13 @@ static ssize_t _send_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_req
 static const coap_resource_t _resources[] = {
     {"/connect", COAP_POST, _connect_handler, NULL},
     {"/disconnect", COAP_POST, _disconnect_handler, NULL},
-    {"/send", COAP_POST, _send_handler, NULL},
+    {"/message", COAP_POST, _message_handler, NULL},
 };
 
 /* Adds link format params to resource list */
 static ssize_t _encode_link(const coap_resource_t *resource, char *buf, size_t maxlen,
                             coap_link_encoder_ctx_t *context) {
   ssize_t res = gcoap_encode_link(resource, buf, maxlen, context);
-  if (res > 0) {
-    if (_link_params[context->link_pos] && (strlen(_link_params[context->link_pos]) < (maxlen - res))) {
-      if (buf) {
-        memcpy(buf + res, _link_params[context->link_pos], strlen(_link_params[context->link_pos]));
-      }
-      return res + strlen(_link_params[context->link_pos]);
-    }
-  }
 
   return res;
 }
@@ -91,7 +146,19 @@ static lf_ret_t CoapChannel_try_connect(NetworkChannel *untyped_self) {
   LF_DEBUG(NET, "CoapChannel: Try connect");
   CoapChannel *self = (CoapChannel *)untyped_self;
 
-  // TODO
+  /* Server */
+  // Do nothing
+
+  /* Client */
+  coap_pkt_t pdu;
+  uint8_t buf[CONFIG_GCOAP_PDU_BUF_SIZE];
+  int len = gcoap_request(&pdu, buf, CONFIG_GCOAP_PDU_BUF_SIZE, COAP_METHOD_POST, "/connect");
+
+  ssize_t bytes_sent = gcoap_req_send(buf, len, &self->remote, NULL, _resp_handler, ctx, tl);
+
+  if (bytes_sent > 0) {
+    // Successfully send
+  }
 }
 
 static void CoapChannel_close_connection(NetworkChannel *untyped_self) {
@@ -115,7 +182,8 @@ static void CoapChannel_register_receive_callback(NetworkChannel *untyped_self,
   LF_INFO(NET, "CoapChannel: Register receive callback");
   CoapChannel *self = (CoapChannel *)untyped_self;
 
-  // TODO
+  self->receive_callback = receive_callback;
+  self->federated_connection = conn;
 }
 
 static void CoapChannel_free(NetworkChannel *untyped_self) {
