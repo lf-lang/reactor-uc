94b0de1e43dee7751d0865f9b318fba319d94c3f
erlingrj <erling.jellum@gmail.com>
2024-11-10 09:41:34 -0800

    Blinky riot

--- examples/riot/blinky/main.c
+++ examples/riot/blinky/main.c
@@ -2,45 +2,38 @@
 #include "reactor-uc/reactor-uc.h"
 #include <inttypes.h>
 
-/* Structs */
-DEFINE_TIMER_STRUCT(MyTimer, 1)
-DEFINE_REACTION_STRUCT(MyReactor, 0, 0)
+DEFINE_TIMER_STRUCT(Blinky, t, 1);
+DEFINE_TIMER_CTOR(Blinky, t, 1);
+DEFINE_REACTION_STRUCT(Blinky, r, 1);
+DEFINE_REACTION_CTOR(Blinky, r, 0);
 
 typedef struct {
   Reactor super;
-  MyReactor_Reaction0 my_reaction;
-  MyTimer timer;
+  TIMER_INSTANCE(Blinky, t);
+  REACTION_INSTANCE(Blinky, r);
+  bool led_on;
   Reaction *_reactions[1];
   Trigger *_triggers[1];
-} MyReactor;
+} Blinky;
 
-/* Reaction Bodies */
-DEFINE_REACTION_BODY(MyReactor, 0) {
-  MyReactor *self = (MyReactor *)_self->parent;
-  Environment *env = self->super.env;
+DEFINE_REACTION_BODY(Blinky, r) {
+  SCOPE_SELF(Blinky);
+  SCOPE_ENV();
+  printf("Hello World @ %lld\n", env->get_elapsed_logical_time(env));
   LED0_TOGGLE;
-  printf("Hello World @ %" PRId64 "\n", env->get_elapsed_physical_time(env));
 }
 
-/* Constructors */
-DEFINE_TIMER_CTOR_FIXED(MyTimer, 1, MSEC(0), MSEC(100))
-DEFINE_REACTION_CTOR(MyReactor, 0)
-
-void MyReactor_ctor(MyReactor *self, Environment *env) {
-  self->_reactions[0] = (Reaction *)&self->my_reaction;
-  self->_triggers[0] = (Trigger *)&self->timer;
-
-  Reactor_ctor(&self->super, "MyReactor", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
-  MyReactor_Reaction0_ctor(&self->my_reaction, &self->super);
-  MyTimer_ctor(&self->timer, &self->super);
-
-  TIMER_REGISTER_EFFECT(self->timer, self->my_reaction);
+void Blinky_ctor(Blinky *self, Environment *env) {
+  Reactor_ctor(&self->super, "Blinky", env, NULL, NULL, 0, self->_reactions, 1, self->_triggers, 1);
+  size_t _triggers_idx = 0;
+  size_t _reactions_idx = 0;
+  INITIALIZE_REACTION(Blinky, r);
+  INITIALIZE_TIMER(Blinky, t, MSEC(0), MSEC(500));
+  TIMER_REGISTER_EFFECT(t, r);
+  self->led_on = false;
 }
 
-/* Application */
-ENTRY_POINT(MyReactor, FOREVER, true)
-
-int main(void) {
+ENTRY_POINT(Blinky, FOREVER, false);
+int main() {
   lf_start();
-  return 0;
 }
