5bd4aa6e9ec6e3222e756045e33d9f17abf43cce
Lasse Rosenow <10547444+LasseRosenow@users.noreply.github.com>
2024-12-06 18:32:17 +0100

    LFC: Add RIOT (#156)
    * WIP
    
    * Add RIOT platform
    
    * Fix makefile generation
    
    * Move some configuration into riot.mk
    
    * Add fallback values
    
    * IT WORKS
    
    * Move more configuration into riot.mk
    
    * Fix old examples
    
    * Fix test
    
    ---------
    
    Co-authored-by: erlingrj <erling.jellum@gmail.com>

--- make/riot/riot.mk
+++ make/riot/riot.mk
@@ -1,7 +1,28 @@
 RIOT_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-# This has to be the absolute path to the RIOT base directory:
-RIOTBASE ?= $(RIOT_MK_DIR)/../../../RIOT
+# Include generated sources and makefiles if SRC_GEN_PATH is defined
+ifdef SRC_GEN_PATH
+include $(SRC_GEN_PATH)/Makefile
+
+# Include generated c files
+SRC += $(patsubst %, $(SRC_GEN_PATH)/%, $(LF_SOURCES)) main.c
+
+# Include generated h files
+CFLAGS += -I$(SRC_GEN_PATH)
+endif
+
+# Check if required environment variables exist
+ifndef RIOTBASE
+$(error RIOTBASE is not defined. Please define it!)
+endif
+
+ifndef EVENT_QUEUE_SIZE
+$(error EVENT_QUEUE_SIZE is not defined. Please define it!)
+endif
+
+ifndef REACTION_QUEUE_SIZE
+$(error REACTION_QUEUE_SIZE is not defined. Please define it!)
+endif
 
 # Comment this out to disable code in RIOT that does safety checking
 # which is not needed in a production environment but helps in the
@@ -14,10 +35,12 @@ QUIET ?= 1
 # Use a peripheral timer for the delay, if available
 FEATURES_OPTIONAL += periph_timer
 
-# External modules
+# Include reactor-uc as an external module
 EXTERNAL_MODULE_DIRS += $(RIOT_MK_DIR)/external_modules
 USEMODULE += reactor-uc
 
-
+# Apply project reactor-uc configuration variables
+CFLAGS += -DEVENT_QUEUE_SIZE=$(EVENT_QUEUE_SIZE)
+CFLAGS += -DREACTION_QUEUE_SIZE=$(REACTION_QUEUE_SIZE)
 
 include $(RIOTBASE)/Makefile.include
