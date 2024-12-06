a694e0bf448d85d4ea558618737b45fa2244d4cc
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-12-06 17:09:48 +0100

    IT WORKS

--- make/riot/riot.mk
+++ make/riot/riot.mk
@@ -1,7 +1,16 @@
 RIOT_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-# This has to be the absolute path to the RIOT base directory:
-RIOTBASE ?= $(RIOT_MK_DIR)/../../../RIOT
+ifndef RIOTBASE
+$(error RIOTBASE is not defined. Please define it!)
+endif
+
+ifndef EVENT_QUEUE_SIZE
+$(error EVENT_QUEUE_SIZE is not defined. Please define it!)
+endif
+
+ifndef REACTION_QUEUE_SIZE
+$(error REACTION_QUEUE_SIZE is not defined. Please define it!)
+endif
 
 # Comment this out to disable code in RIOT that does safety checking
 # which is not needed in a production environment but helps in the
@@ -19,8 +28,6 @@ EXTERNAL_MODULE_DIRS += $(RIOT_MK_DIR)/external_modules
 USEMODULE += reactor-uc
 
 # Apply project reactor-uc configuration variables
-EVENT_QUEUE_SIZE ?= 1
-REACTION_QUEUE_SIZE ?= 1
 CFLAGS += -DEVENT_QUEUE_SIZE=$(EVENT_QUEUE_SIZE)
 CFLAGS += -DREACTION_QUEUE_SIZE=$(REACTION_QUEUE_SIZE)
 
