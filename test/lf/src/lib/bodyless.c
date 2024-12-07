f211a775dddae9cdd76d1b076dc3aab8842d69e6
erlingrj <erling.jellum@gmail.com>
2024-12-06 20:15:08 -0800

    Fix

--- test/lf/src/lib/bodyless.c
+++ test/lf/src/lib/bodyless.c
@@ -1,4 +1,4 @@
-#include "Bodiless/Bodiless.h"
+#include "Bodyless/Bodyless.h"
 
 void Reactor_Bodiless_hello(Reactor_Bodiless *self, Environment *env, const Reactor_Bodiless_Startup *startup) {
   printf("Hello, World!\n");
