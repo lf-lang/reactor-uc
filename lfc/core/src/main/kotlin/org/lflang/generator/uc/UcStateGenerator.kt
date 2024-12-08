714c00cc3629a792f674fdf1e188595249fd7e26
erlingrj <erling.jellum@gmail.com>
2024-12-08 08:28:07 -0800

    Refactor

--- lfc/core/src/main/kotlin/org/lflang/generator/uc/UcStateGenerator.kt
+++ lfc/core/src/main/kotlin/org/lflang/generator/uc/UcStateGenerator.kt
@@ -1,18 +1,14 @@
 package org.lflang.generator.uc
 
 import org.lflang.allStateVars
+import org.lflang.generator.uc.UcPortGenerator.Companion.arrayLength
+import org.lflang.generator.uc.UcPortGenerator.Companion.isArray
 import org.lflang.isInitialized
 import org.lflang.lf.Reactor
 import org.lflang.lf.StateVar
 import org.lflang.toText
 
 class UcStateGenerator(private val reactor: Reactor) {
-    private val StateVar.isArray
-        get(): Boolean = type.cStyleArraySpec != null
-
-    private val StateVar.arrayLength
-        get(): Int = type.cStyleArraySpec.length
-
     fun generateReactorStructFields() =
         reactor.allStateVars.joinToString(prefix = "// State variables \n", separator = "\n") {
             if (it.isArray) {
@@ -24,8 +20,8 @@ class UcStateGenerator(private val reactor: Reactor) {
 
     fun generateInitializeStateVars() =
         reactor.allStateVars.filter{it.isInitialized}.joinToString(prefix = "// Initialize State variables \n", separator = "\n") {
-            if (it.isArray) {
-                """|${it.type.id} _${it.name}_init[${it.arrayLength}] = ${it.init.expr.toCCode()};
+            if (it.type.isArray) {
+                """|${it.type.id} _${it.name}_init[${it.type.arrayLength}] = ${it.init.expr.toCCode()};
                    |memcpy(&self->${it.name}, &_${it.name}_init, sizeof(_${it.name}_init));
                 """.trimMargin()
             } else {
