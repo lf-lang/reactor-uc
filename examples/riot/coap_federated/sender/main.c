b3cb4b8c259b231f74c4e9ff2f0ff9faac7966b8
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-12-10 22:12:52 +0100

    Fix after merging main

--- examples/riot/coap_federated/sender/main.c
+++ examples/riot/coap_federated/sender/main.c
@@ -19,25 +19,25 @@ size_t serialize_msg_t(const void *user_struct, size_t user_struct_size, unsigne
   return sizeof(msg->size) + msg->size;
 }
 
-DEFINE_TIMER_STRUCT(Sender, t, 1, 0)
-DEFINE_TIMER_CTOR(Sender, t, 1, 0)
-DEFINE_REACTION_STRUCT(Sender, r, 1)
-DEFINE_REACTION_CTOR(Sender, r, 0)
-DEFINE_OUTPUT_STRUCT(Sender, out, 1, lf_msg_t)
-DEFINE_OUTPUT_CTOR(Sender, out, 1)
+LF_DEFINE_TIMER_STRUCT(Sender, t, 1, 0)
+LF_DEFINE_TIMER_CTOR(Sender, t, 1, 0)
+LF_DEFINE_REACTION_STRUCT(Sender, r, 1)
+LF_DEFINE_REACTION_CTOR(Sender, r, 0)
+LF_DEFINE_OUTPUT_STRUCT(Sender, out, 1, lf_msg_t)
+LF_DEFINE_OUTPUT_CTOR(Sender, out, 1)
 
 typedef struct {
   Reactor super;
-  TIMER_INSTANCE(Sender, t);
-  REACTION_INSTANCE(Sender, r);
-  PORT_INSTANCE(Sender, out, 1);
-  REACTOR_BOOKKEEPING_INSTANCES(1, 2, 0);
+  LF_TIMER_INSTANCE(Sender, t);
+  LF_REACTION_INSTANCE(Sender, r);
+  LF_PORT_INSTANCE(Sender, out, 1);
+  LF_REACTOR_BOOKKEEPING_INSTANCES(1, 2, 0);
 } Sender;
 
-DEFINE_REACTION_BODY(Sender, r) {
-  SCOPE_SELF(Sender);
-  SCOPE_ENV();
-  SCOPE_PORT(Sender, out);
+LF_DEFINE_REACTION_BODY(Sender, r) {
+  LF_SCOPE_SELF(Sender);
+  LF_SCOPE_ENV();
+  LF_SCOPE_PORT(Sender, out);
 
   printf("Timer triggered @ %" PRId64 "\n", env->get_elapsed_logical_time(env));
   lf_msg_t val;
@@ -46,53 +46,54 @@ DEFINE_REACTION_BODY(Sender, r) {
   lf_set(out, val);
 }
 
-REACTOR_CTOR_SIGNATURE_WITH_PARAMETERS(Sender, OutputExternalCtorArgs *out_external) {
-  REACTOR_CTOR_PREAMBLE();
-  REACTOR_CTOR(Sender);
-  INITIALIZE_REACTION(Sender, r);
-  INITIALIZE_TIMER(Sender, t, MSEC(0), SEC(1));
-  INITIALIZE_OUTPUT(Sender, out, 1, out_external);
+LF_REACTOR_CTOR_SIGNATURE_WITH_PARAMETERS(Sender, OutputExternalCtorArgs *out_external) {
+  LF_REACTOR_CTOR_PREAMBLE();
+  LF_REACTOR_CTOR(Sender);
+  LF_INITIALIZE_REACTION(Sender, r);
+  LF_INITIALIZE_TIMER(Sender, t, MSEC(0), SEC(1));
+  LF_INITIALIZE_OUTPUT(Sender, out, 1, out_external);
 
-  TIMER_REGISTER_EFFECT(self->t, self->r);
-  PORT_REGISTER_SOURCE(self->out, self->r, 1);
+  LF_TIMER_REGISTER_EFFECT(self->t, self->r);
+  LF_PORT_REGISTER_SOURCE(self->out, self->r, 1);
 }
 
-DEFINE_FEDERATED_OUTPUT_CONNECTION(Sender, out, lf_msg_t, 1)
+LF_DEFINE_FEDERATED_OUTPUT_CONNECTION(Sender, out, lf_msg_t, 1)
 
 typedef struct {
   FederatedConnectionBundle super;
   CoapUdpIpChannel channel;
-  FEDERATED_OUTPUT_CONNECTION_INSTANCE(Sender, out);
-  FEDERATED_CONNECTION_BUNDLE_BOOKKEEPING_INSTANCES(0, 1);
-} FEDERATED_CONNECTION_BUNDLE_NAME(Sender, Receiver);
+  LF_FEDERATED_OUTPUT_CONNECTION_INSTANCE(Sender, out);
+  LF_FEDERATED_CONNECTION_BUNDLE_BOOKKEEPING_INSTANCES(0, 1);
+} LF_FEDERATED_CONNECTION_BUNDLE_NAME(Sender, Receiver);
 
-FEDERATED_CONNECTION_BUNDLE_CTOR_SIGNATURE(Sender, Receiver) {
-  FEDERATED_CONNECTION_BUNDLE_CTOR_PREAMBLE();
+LF_FEDERATED_CONNECTION_BUNDLE_CTOR_SIGNATURE(Sender, Receiver) {
+  LF_FEDERATED_CONNECTION_BUNDLE_CTOR_PREAMBLE();
   CoapUdpIpChannel_ctor(&self->channel, parent->env, REMOTE_ADDRESS, REMOTE_PROTOCOL_FAMILY);
-  FEDERATED_CONNECTION_BUNDLE_CALL_CTOR();
-  INITIALIZE_FEDERATED_OUTPUT_CONNECTION(Sender, out, serialize_msg_t);
+  LF_FEDERATED_CONNECTION_BUNDLE_CALL_CTOR();
+  LF_INITIALIZE_FEDERATED_OUTPUT_CONNECTION(Sender, out, serialize_msg_t);
 }
 
 // Reactor main
 typedef struct {
   Reactor super;
-  CHILD_REACTOR_INSTANCE(Sender, sender, 1);
-  FEDERATED_CONNECTION_BUNDLE_INSTANCE(Sender, Receiver);
-  FEDERATE_BOOKKEEPING_INSTANCES(1);
-  CHILD_OUTPUT_CONNECTIONS(sender, out, 1, 1, 1);
-  CHILD_OUTPUT_EFFECTS(sender, out, 1, 1, 0);
-  CHILD_OUTPUT_OBSERVERS(sender, out, 1, 1, 0);
+  LF_CHILD_REACTOR_INSTANCE(Sender, sender, 1);
+  LF_FEDERATED_CONNECTION_BUNDLE_INSTANCE(Sender, Receiver);
+  LF_FEDERATE_BOOKKEEPING_INSTANCES(1);
+  LF_CHILD_OUTPUT_CONNECTIONS(sender, out, 1, 1, 1);
+  LF_CHILD_OUTPUT_EFFECTS(sender, out, 1, 1, 0);
+  LF_CHILD_OUTPUT_OBSERVERS(sender, out, 1, 1, 0);
 } MainSender;
 
-REACTOR_CTOR_SIGNATURE(MainSender) {
-  REACTOR_CTOR(MainSender);
-  FEDERATE_CTOR_PREAMBLE();
-  DEFINE_CHILD_OUTPUT_ARGS(sender, out, 1, 1);
-  INITIALIZE_CHILD_REACTOR_WITH_PARAMETERS(Sender, sender, 1, _sender_out_args[i]);
-  INITIALIZE_FEDERATED_CONNECTION_BUNDLE(Sender, Receiver);
-  BUNDLE_REGISTER_UPSTREAM(Sender, Receiver, sender, out);
+LF_REACTOR_CTOR_SIGNATURE(MainSender) {
+  LF_REACTOR_CTOR(MainSender);
+  LF_FEDERATE_CTOR_PREAMBLE();
+  LF_DEFINE_CHILD_OUTPUT_ARGS(sender, out, 1, 1);
+  LF_INITIALIZE_CHILD_REACTOR_WITH_PARAMETERS(Sender, sender, 1, _sender_out_args[i]);
+  LF_INITIALIZE_FEDERATED_CONNECTION_BUNDLE(Sender, Receiver);
+  LF_BUNDLE_REGISTER_UPSTREAM(Sender, Receiver, sender, out);
 }
-ENTRY_POINT_FEDERATED(MainSender, SEC(1), true, false, 1, true)
+
+LF_ENTRY_POINT_FEDERATED(MainSender, SEC(1), true, false, 1, true)
 
 void print_ip_addresses(void) {
   gnrc_netif_t *netif = gnrc_netif_iter(NULL);
