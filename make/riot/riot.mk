598397fa4cfa6c5d11abde38e143227e8e62c44f
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-12-13 13:39:16 +0100

    Move lfc back to template

--- make/riot/riot.mk
+++ make/riot/riot.mk
@@ -1,52 +1,16 @@
 RIOT_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-# Include generated sources and makefiles if LF_MAIN is defined
-ifdef LF_MAIN
-# Execute the LF compiler
-.PHONY: lfcg
-lfcg:
-	$(REACTOR_UC_PATH)/lfc/bin/lfc-dev src/$(LF_MAIN).lf
-
-all: lfcg
-
-# Check if REACTOR_UC_PATH variable exist
-ifndef REACTOR_UC_PATH
-$(error REACTOR_UC_PATH is not defined. Please define it!)
-endif
-
-# Name of your RIOT application
-APPLICATION ?= $(LF_MAIN)
-
-# This has to be the absolute path to the RIOT base directory:
-RIOTBASE ?= $(CURDIR)/RIOT
-
-# Path of generated lf c-code
-LF_SRC_GEN_PATH ?= $(CURDIR)/src-gen/$(LF_MAIN)
-
-# Include the Makefile of the generated target application
-include $(LF_SRC_GEN_PATH)/Makefile
-
-# Include generated c files
-SRC += $(patsubst %, $(LF_SRC_GEN_PATH)/%, $(LFC_GEN_SOURCES))
-
-# Include generated main file
-SRC += $(LF_SRC_GEN_PATH)/${LFC_GEN_MAIN}
-
-# Include generated h files
-CFLAGS += -I$(LF_SRC_GEN_PATH)
-endif
-
 # Check if required environment variables exist
 ifndef RIOTBASE
-$(error RIOTBASE is not defined. Please define it!)
+  $(error RIOTBASE is not defined. Please define it!)
 endif
 
 ifndef EVENT_QUEUE_SIZE
-$(error EVENT_QUEUE_SIZE is not defined. Please define it!)
+  $(error EVENT_QUEUE_SIZE is not defined. Please define it!)
 endif
 
 ifndef REACTION_QUEUE_SIZE
-$(error REACTION_QUEUE_SIZE is not defined. Please define it!)
+  $(error REACTION_QUEUE_SIZE is not defined. Please define it!)
 endif
 
 # Comment this out to disable code in RIOT that does safety checking
