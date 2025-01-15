120b439049431bb8fdb12459eb6127983ac100df
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2025-01-15 18:26:00 +0100

    Use tmux

--- test/platform/riot/coap_channel_federated_test/run.sh
+++ test/platform/riot/coap_channel_federated_test/run.sh
@@ -3,47 +3,47 @@
 # Create tap interfaces
 # TODO
 
-# ---------------------------------------------------------------
+# --------------------------- Print IP-Addresses ---------------------------
 
-# # Print IP-Addresses
+# Build sender
+make REMOTE_ADDRESS=fe80::8cc3:33ff:febb:1b3 BOARD=native PORT=tap0 all -C ./sender
 
-## Build sender
-# pushd sender
-# make ONLY_PRINT_IP=1 BOARD=native PORT=tap0 all
-# popd
-
-# ## Build receiver
-# pushd receiver
-# make ONLY_PRINT_IP=1 BOARD=native PORT=tap1 all
-# popd
-
-# ## Run sender and receiver
-# ./sender/bin/native/*.elf tap0 &
-# ./receiver/bin/native/*.elf tap1 &
-
-# ---------------------------------------------------------------
-
-# # Build sender
-# pushd sender
-# make REMOTE_ADDRESS=fe80::8cc3:33ff:febb:1b3 BOARD=native PORT=tap0 all
-# popd
-
-# # Build receiver
-# pushd receiver
-# make REMOTE_ADDRESS=fe80::44e5:1bff:fee4:dac8 BOARD=native PORT=tap1 all
-# popd
+# Build receiver
+make REMOTE_ADDRESS=fe80::44e5:1bff:fee4:dac8 BOARD=native PORT=tap1 all -C ./receiver
 
 # Run sender and receiver
-( ./sender/bin/native/*.elf tap0 ) &
-( ./receiver/bin/native/*.elf tap1 ) &
+./sender/bin/native/*.elf tap0 &
+./receiver/bin/native/*.elf tap1 &
 
-# Wait for both tests to finish
-wait
+# --------------------------- Build and run test --------------------------- 
+
+# Build sender
+make REMOTE_ADDRESS=fe80::8cc3:33ff:febb:1b3 BOARD=native PORT=tap0 all -C ./sender
+
+# Build receiver
+make REMOTE_ADDRESS=fe80::44e5:1bff:fee4:dac8 BOARD=native PORT=tap1 all -C ./receiver
+
+SESSION_NAME="federated_coap_test"
+
+# Create a new tmux session
+tmux new-session -d -s $SESSION_NAME
+
+# Run the sender
+tmux send-keys -t $SESSION_NAME "./sender/bin/native/*.elf tap0" C-m
+
+sleep 3
+
+# Run the receiver
+tmux split-window -h -t $SESSION_NAME
+tmux send-keys -t $SESSION_NAME "./receiver/bin/native/*.elf tap1" C-m
+
+# Attach to the tmux session
+tmux attach-session -t $SESSION_NAME
 
 # Evaluate test output
-if [ $? -eq 0 ]; then
-    echo "All tests passed."
-else
-    echo "$? tests failed." >&2
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "All tests passed."
+# else
+#     echo "$? tests failed." >&2
+#     exit 1
+# fi
