ce2adb95dfebeee6d44322e0bf94a71a6dd890db
tanneberger <github@tanneberger.me>
2025-01-17 02:11:46 +0100

    Sync and Async UART Implementation

--- include/reactor-uc/platform/riot/uart_channel.h
+++ include/reactor-uc/platform/riot/uart_channel.h
@@ -9,31 +9,38 @@
 #include "periph/uart.h"
 #include "cond.h"
 
-typedef struct UARTChannel UARTChannel;
 typedef struct FederatedConnectionBundle FederatedConnectionBundle;
+typedef struct UARTSyncChannel UARTSyncChannel;
+typedef struct UARTAsyncChannel UARTAsyncChannel;
 
 #define UART_CHANNEL_BUFFERSIZE 1024
 #define UART_CHANNEL_EXPECTED_CONNECT_DURATION MSEC(10) // TODO:
 
-struct UARTChannel {
-  NetworkChannel super;
+struct UARTSyncChannel {
+  SyncNetworkChannel super;
   NetworkChannelState state;
 
   FederateMessage output;
   unsigned char write_buffer[UART_CHANNEL_BUFFERSIZE];
   unsigned char read_buffer[UART_CHANNEL_BUFFERSIZE];
-  char connection_thread_stack[THREAD_STACKSIZE_MAIN];
-  int connection_thread_pid;
-
   unsigned int read_index;
   uart_t uart_dev;
-  mutex_t receive_lock;
-  cond_t receive_cv;
 
   FederatedConnectionBundle *federated_connection;
   void (*receive_callback)(FederatedConnectionBundle *conn, const FederateMessage *message);
 };
 
-void UARTChannel_ctor(UARTChannel *self, Environment *env, uint32_t baud);
+struct UARTAsyncChannel {
+  UARTSyncChannel super;
+
+  char connection_thread_stack[THREAD_STACKSIZE_MAIN];
+  int connection_thread_pid;
+  mutex_t receive_lock;
+  cond_t receive_cv;
+};
+
+void UARTSyncChannel_ctor(UARTSyncChannel *self, Environment *env, uint32_t baud);
+
+void UARTAsyncChannel_ctor(UARTAsyncChannel *self, Environment *env, uint32_t baud);
 
 #endif
