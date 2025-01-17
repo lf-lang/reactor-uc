83a05eef25c8febb4a679d60cdcd2e112e79f4ac
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2025-01-17 16:18:49 +0100

    Create tap interfaces

--- test/platform/riot/coap_channel_federated_test/run.sh
+++ test/platform/riot/coap_channel_federated_test/run.sh
@@ -4,7 +4,7 @@ SENDER_IP=fe80::44e5:1bff:fee4:dac8
 RECEIVER_IP=fe80::8cc3:33ff:febb:1b3
 
 # Create tap interfaces
-# TODO
+sudo $RIOTBASE/dist/tools/tapsetup/tapsetup
 
 # --------------------------- Print IP-Addresses ---------------------------
 
