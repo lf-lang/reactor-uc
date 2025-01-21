823b923ff628ed07c69b8fdba1813d57f09b3f0c
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2025-01-21 17:44:45 +0100

    Add all riot examples to the CI again

---
+++ examples/riot/coap_federated/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+REMOTE_ADDRESS=fe80::8cc3:33ff:febb:1b3 make all -C ./sender
+REMOTE_ADDRESS=fe80::8cc3:33ff:febb:1b3 make all -C ./receiver
---
+++ examples/riot/coap_federated_lf/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+FEDERATION=r1 make all
+FEDERATION=r2 make all
