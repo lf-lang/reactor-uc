2809b1bb518a481c5f9bd6c1a6d176dbc26a46c0
tanneberger <github@tanneberger.me>
2025-01-27 02:34:45 +0100

    formatting

--- lfc/core/src/main/java/org/lflang/validation/AttributeSpec.java
+++ lfc/core/src/main/java/org/lflang/validation/AttributeSpec.java
@@ -255,16 +255,15 @@ public class AttributeSpec {
     // address="127.0.0.1")
 
     ATTRIBUTE_SPECS_BY_NAME.put(
-       "interface_uart",
-      new AttributeSpec(
-                    List.of(
-                            new AttrParamSpec("name", AttrParamType.STRING, true),
-                            new AttrParamSpec("uart_device", AttrParamType.INT, true),
-                            new AttrParamSpec("baud_rate", AttrParamType.INT, true),
-                            new AttrParamSpec("data_bits", AttrParamType.STRING, true),
-                            new AttrParamSpec("parity", AttrParamType.STRING, true),
-                            new AttrParamSpec("stop_bits", AttrParamType.STRING, true)
-                            )));
+        "interface_uart",
+        new AttributeSpec(
+            List.of(
+                new AttrParamSpec("name", AttrParamType.STRING, true),
+                new AttrParamSpec("uart_device", AttrParamType.INT, true),
+                new AttrParamSpec("baud_rate", AttrParamType.INT, true),
+                new AttrParamSpec("data_bits", AttrParamType.STRING, true),
+                new AttrParamSpec("parity", AttrParamType.STRING, true),
+                new AttrParamSpec("stop_bits", AttrParamType.STRING, true))));
 
     ATTRIBUTE_SPECS_BY_NAME.put(
         "interface_tcp",
--- lfc/core/src/main/kotlin/org/lflang/generator/uc/UcIpAddress.kt
+++ lfc/core/src/main/kotlin/org/lflang/generator/uc/UcIpAddress.kt
@@ -129,4 +129,3 @@ object IpAddressManager {
   }
 }
 class UcIpAddress { }
-
