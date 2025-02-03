22a2d2beb1deba9dcdc7ad5c7ecba23b26506ebc
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2025-02-03 19:43:09 +0100

    Remove makefile from coap-federated example

--- examples/riot/coap_federated_lf/build.sh
+++ examples/riot/coap_federated_lf/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-make all
+LF_MAIN=CoapFederatedLF
+
+$REACTOR_UC_PATH/lfc/bin/lfc-dev --gen-fed-templates src/$LF_MAIN.lf
 
 PORT=tap0 make all -C ./CoapFederatedLF/r1
 PORT=tap1 make all -C ./CoapFederatedLF/r2
