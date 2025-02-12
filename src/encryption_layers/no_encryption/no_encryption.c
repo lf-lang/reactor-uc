84294ae9c2d0827e7495fd64f0083a5f321a99a4
tanneberger <github@tanneberger.me>
2025-02-12 03:44:20 +0100

    adjusting test

--- src/encryption_layers/no_encryption/no_encryption.c
+++ src/encryption_layers/no_encryption/no_encryption.c
@@ -3,8 +3,13 @@
 //
 
 #include "reactor-uc/encryption_layers/no_encryption/no_encryption.h"
-
+#include "reactor-uc/logging.h"
 #include "reactor-uc/serialization.h"
+
+#define NO_ENCRYPTION_LAYER_ERR(fmt, ...) LF_ERR(NET, "NoEncryptionLayer: " fmt, ##__VA_ARGS__)
+#define NO_ENCRYPTION_LAYER_WARN(fmt, ...) LF_WARN(NET, "NoEncryptionLayer: " fmt, ##__VA_ARGS__)
+#define NO_ENCRYPTION_LAYER_INFO(fmt, ...) LF_INFO(NET, "NoEncryptionLayer: " fmt, ##__VA_ARGS__)
+#define NO_ENCRYPTION_LAYER_DEBUG(fmt, ...) LF_DEBUG(NET, "NoEncryptionLayer: " fmt, ##__VA_ARGS__)
 
 lf_ret_t NoEncryptionLayer_send_message(EncryptionLayer *untyped_self, const FederateMessage *msg) {
   NoEncryptionLayer *self = (NoEncryptionLayer *)untyped_self;
@@ -30,30 +35,34 @@ void NoEncryptionLayer_register_callback(EncryptionLayer *untyped_self,
   self->receive_callback = receive_callback;
 }
 
-int _NoEncryptionLayer_receive_callback(EncryptionLayer *untyped_self, char *buffer, ssize_t buffer_size) {
+void _NoEncryptionLayer_receive_callback(EncryptionLayer *untyped_self, const char *buffer, ssize_t buffer_size) {
   (void)buffer_size; // TODO: FIXME
   NoEncryptionLayer *self = (NoEncryptionLayer *)untyped_self;
   const MessageFraming *message_framing = (MessageFraming *)buffer;
 
+  NO_ENCRYPTION_LAYER_DEBUG("Received Callback from NetworkChannel Buffer Size: %d", buffer_size);
+
   if (validate_message_framing((unsigned char *)buffer, UC_NO_ENCRYPTION) != LF_OK) {
+    NO_ENCRYPTION_LAYER_ERR("Invalid Message Framing");
   }
 
-  int consumed_bytes = deserialize_from_protobuf(&self->msg, (unsigned char *)buffer, message_framing->message_size);
+  int consumed_bytes = deserialize_from_protobuf(&self->msg, (unsigned char *)buffer + sizeof(MessageFraming),
+                                                 message_framing->message_size);
 
   if (consumed_bytes < 0) {
-    // ERROR
+    NO_ENCRYPTION_LAYER_ERR("Consumed Bytes is zero");
   }
 
+  NO_ENCRYPTION_LAYER_DEBUG("Calling Receive Callback");
+  if (self->receive_callback == NULL) {
+    NO_ENCRYPTION_LAYER_ERR("NoEncryptionLayer callback is zero!");
+  }
   self->receive_callback(self->bundle, &self->msg);
-
-  return consumed_bytes + sizeof(MessageFraming);
 }
 
 void NoEncryptionLayer_ctor(NoEncryptionLayer *self, NetworkChannel *network_channel) {
   self->network_channel = network_channel;
   self->super.send_message = NoEncryptionLayer_send_message;
   self->super.register_receive_callback = NoEncryptionLayer_register_callback;
-
-  network_channel->register_encryption_layer(network_channel, (EncryptionLayer *)self,
-                                             _NoEncryptionLayer_receive_callback);
+  network_channel->register_receive_callback(network_channel, &self->super, _NoEncryptionLayer_receive_callback);
 }
