5450bbb7ee74c3237cf6ed0e6141a131b56d7a2c
Ehsan Khodadad <ehkh@dtu.dk>
2025-05-22 14:53:25 +0200

    Patmos and S4NoC: 3rd Commit

--- src/platform/patmos/s4noc_channel.c
+++ src/platform/patmos/s4noc_channel.c
@@ -1,13 +1,14 @@
-#include "reactor-uc/platform/patmos/s4noc_channel.h"
 #include "reactor-uc/logging.h"
 #include "reactor-uc/serialization.h"
-#include "machine/patmos.h"
-#include <arpa/inet.h>
+#include <machine/patmos.h>
+#include <stdint.h>
 
 #define S4NOC_CHANNEL_ERR(fmt, ...) LF_ERR(NET, "S4NOCPollChannel: " fmt, ##__VA_ARGS__)
 #define S4NOC_CHANNEL_WARN(fmt, ...) LF_WARN(NET, "S4NOCPollChannel: " fmt, ##__VA_ARGS__)
 #define S4NOC_CHANNEL_INFO(fmt, ...) LF_INFO(NET, "S4NOCPollChannel: " fmt, ##__VA_ARGS__)
 #define S4NOC_CHANNEL_DEBUG(fmt, ...) LF_DEBUG(NET, "S4NOCPollChannel: " fmt, ##__VA_ARGS__)
+
+S4NOCGlobalState s4noc_global_state;
 
 static lf_ret_t S4NOCPollChannel_open_connection(NetworkChannel *untyped_self) {
   S4NOC_CHANNEL_DEBUG("Open connection");
@@ -38,16 +39,16 @@ static lf_ret_t S4NOCPollChannel_send_blocking(NetworkChannel *untyped_self, con
   volatile _IODEV int *s4noc_dest = (volatile _IODEV int *)(PATMOS_IO_S4NOC + 8);
 
   if (self->state == NETWORK_CHANNEL_STATE_CONNECTED) {
-    int message_size = serialize_message(message, self->write_buffer, S4NOC_CHANNEL_BUFFERSIZE);
-    S4NOC_CHANNEL_DEBUG("Sending Message of Size: %i", message_size);
-
+    int message_size = serialize_to_protobuf(message, self->write_buffer, S4NOC_CHANNEL_BUFFERSIZE);
+    
     *s4noc_dest = self->destination_core;
+    S4NOC_CHANNEL_DEBUG("Sending Message of Size: %i to %i", message_size, *s4noc_dest);
     int bytes_send = 0;
     while (bytes_send < message_size) {
       *s4noc_data = ((int *)self->write_buffer)[bytes_send / 4];
       bytes_send += 4;
     }
-
+    S4NOC_CHANNEL_DEBUG("Sent %d bytes", bytes_send);
     return LF_OK;
   } else {
     return LF_ERR;
@@ -65,33 +66,41 @@ static void S4NOCPollChannel_register_receive_callback(NetworkChannel *untyped_s
   self->federated_connection = conn;
 }
 
+uint32_t ntohl(uint32_t netlong) {
+  return ((netlong & 0xFF000000) >> 24) |
+         ((netlong & 0x00FF0000) >> 8) |
+         ((netlong & 0x0000FF00) << 8) |
+         ((netlong & 0x000000FF) << 24);
+}
+
 void S4NOCPollChannel_poll(NetworkChannel *untyped_self) {
   S4NOCPollChannel *self = (S4NOCPollChannel *)untyped_self;
+  LF_INFO(NET, "S4NOCPollChannel_poll called");
 
   volatile _IODEV int *s4noc_status = (volatile _IODEV int *)PATMOS_IO_S4NOC;
   volatile _IODEV int *s4noc_data = (volatile _IODEV int *)(PATMOS_IO_S4NOC + 4);
   volatile _IODEV int *s4noc_source = (volatile _IODEV int *)(PATMOS_IO_S4NOC + 8);
 
   if (((*s4noc_status) & 0x02) == 0) {
+    LF_INFO(NET, "S4NOCPollChannel_poll: No data available");
     return;
   }
 
   int value = *s4noc_data;
   int source = *s4noc_source;
-
+  LF_INFO(NET, "S4NOCPollChannel_poll: Received value %d from source %d", value, source);
   S4NOCPollChannel *receive_channel = s4noc_global_state.core_channels[get_cpuid()][source];
 
   ((int *)receive_channel->receive_buffer)[receive_channel->receive_buffer_index / 4] = value;
   receive_channel->receive_buffer_index += 4;
 
-  int expected_message_size = ntohl(*((int *)receive_channel->receive_buffer));
+  unsigned int expected_message_size = ntohl(*((int *)receive_channel->receive_buffer));
   if (expected_message_size + 4 > receive_channel->receive_buffer_index) {
     int bytes_left = deserialize_from_protobuf(&receive_channel->output, receive_channel->receive_buffer + 1,
                                                self->receive_buffer_index - 4);
     S4NOC_CHANNEL_DEBUG("Bytes Left after attempted to deserialize %d", bytes_left);
 
     if (bytes_left >= 0) {
-      int receive_buffer_index = receive_channel->receive_buffer_index;
       receive_channel->receive_buffer_index = bytes_left;
 
       if (receive_channel->receive_callback != NULL) {
@@ -106,7 +115,7 @@ void S4NOCPollChannel_ctor(S4NOCPollChannel *self, Environment *env, unsigned in
   assert(self != NULL);
   assert(env != NULL);
 
-  self->super.super.mode = NETWORK_CHANNEL_MODE_POLL;
+  self->super.super.mode = NETWORK_CHANNEL_MODE_POLLED;
   self->super.super.expected_connect_duration = SEC(0);
   self->super.super.type = NETWORK_CHANNEL_TYPE_S4NOC;
   self->super.super.is_connected = S4NOCPollChannel_is_connected;
