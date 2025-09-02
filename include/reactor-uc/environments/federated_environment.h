427b6170daabfd3d368f3c1a496038d5d72078db
fra-p <francesco.paladino.25@gmail.com>
2025-09-02 18:52:48 +0200

    Runtime: implemented API to dynamically set federate maxwait
    1. Implemented an API call to allow reactions set the maxwait parameter
    of the decentralized coordination while the LF program executes.

--- include/reactor-uc/environments/federated_environment.h
+++ include/reactor-uc/environments/federated_environment.h
@@ -20,11 +20,23 @@ struct FederatedEnvironment {
   size_t federation_longest_path;          // The longest path in the federation.
   StartupCoordinator *startup_coordinator; // A pointer to the startup coordinator, if the program has one.
   ClockSynchronization *clock_sync;        // A pointer to the clock synchronization module, if the program has one.
+  interval_t global_max_wait;              // The global maximum wait time for remote input ports for this federate.
+
+  /**
+   * @brief Set the global maximum wait time for remote input ports for this federate.
+   * @param super The environment.
+   * @param max_wait The maximum wait time to be set.
+   *
+   * This function sets the global maximum wait time for remote input ports for this federate.
+   */
+  void (*set_maxwait)(Environment *super, interval_t max_wait);
 };
 
 void FederatedEnvironment_ctor(FederatedEnvironment *self, Reactor *main, Scheduler *scheduler, bool fast_mode,
                                FederatedConnectionBundle **net_bundles, size_t net_bundles_size,
                                StartupCoordinator *startup_coordinator, ClockSynchronization *clock_sync);
 void FederatedEnvironment_free(FederatedEnvironment *self);
+
+#define lf_set_maxwait(time) ((FederatedEnvironment *)env)->set_maxwait(env, time)
 
 #endif
