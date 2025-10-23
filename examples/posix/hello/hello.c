8f09e1574f89e1492025598be9f19930ae4fce6b
Sebastiano <gaia.franseba@gmail.com>
2025-10-23 15:58:50 +0200

    Add TimerSource startup reaction

--- examples/posix/hello/hello.c
+++ examples/posix/hello/hello.c
@@ -7,6 +7,14 @@ LF_DEFINE_REACTION_BODY(TimerSource, r) {
   printf("TimerSource World @ " PRINTF_TIME "\n", env->get_elapsed_logical_time(env));
 }
 
+LF_DEFINE_REACTION_BODY(TimerSource, s) {
+  LF_SCOPE_SELF(TimerSource);
+  LF_SCOPE_ENV();
+  LF_SCOPE_STARTUP(TimerSource);
+  printf("TimerSource Startup @ " PRINTF_TIME "\n", env->get_elapsed_logical_time(env));
+}
+
+
 int main() {
   lf_start();
 }
