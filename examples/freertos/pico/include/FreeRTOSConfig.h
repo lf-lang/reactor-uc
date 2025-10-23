851751d57f65619ae07a62b8e6a1df0536c42509
Sebastiano <gaia.franseba@gmail.com>
2025-10-23 15:48:14 +0200

    Enable tick hook in FreeRTOSConfig

--- examples/freertos/pico/include/FreeRTOSConfig.h
+++ examples/freertos/pico/include/FreeRTOSConfig.h
@@ -43,7 +43,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
-#define configUSE_TICK_HOOK                     0
+#define configUSE_TICK_HOOK                     1
 #define configCPU_CLOCK_HZ                      ( 133000000 ) 
 #define configTICK_RATE_HZ                      ( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES                    32
