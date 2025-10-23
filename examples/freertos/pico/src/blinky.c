7b593ba4a640a84d68bdafafd8e40733109b3a47
Sebastiano <gaia.franseba@gmail.com>
2025-10-23 15:49:08 +0200

    Refactor pico_toggle_led invoked in startup reaction

--- examples/freertos/pico/src/blinky.c
+++ examples/freertos/pico/src/blinky.c
@@ -24,9 +24,10 @@ int pico_led_init(void) {
 }
 
 // Toggle the led
-void pico_toggle_led() {
+void pico_toggle_led(Environment *env) {
  static bool led_on = false;
  led_on = !led_on;
+ printf("Turning LED %s, @ lt=%lld, pt=%lld\n", led_on ? "ON" : "OFF", env->get_elapsed_logical_time(env), env->get_physical_time(env));
 #if defined(PICO_DEFAULT_LED_PIN)
   // Just set the GPIO on or off
   gpio_put(PICO_DEFAULT_LED_PIN, led_on);
@@ -41,12 +42,19 @@ void pico_toggle_led() {
 LF_DEFINE_REACTION_BODY(TimerSource, r) {
   LF_SCOPE_SELF(TimerSource);
   LF_SCOPE_ENV();
-  printf("Hello World, @lt=%lld\n - @pt=%lld\n", env->get_elapsed_logical_time(env), env->get_physical_time(env));
-  pico_toggle_led();
+  pico_toggle_led(env);
+}
+
+LF_DEFINE_REACTION_BODY(TimerSource, s) {
+  LF_SCOPE_SELF(TimerSource);
+  LF_SCOPE_ENV();
+  LF_SCOPE_STARTUP(TimerSource);
+  // Configure the LED on startup
+  printf("Initializing LED\n");
+  pico_led_init();
 }
 
 void main_pico() {
-  pico_led_init();
   lf_start();
 }
 
