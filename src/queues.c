97eef9f170600307a7cedb220fa395be753d6539
fra-p <francesco.paladino.25@gmail.com>
2026-02-03 23:46:34 +0100

    Fixed bug with reactions triggered my multiple inputs
    1. Fixed a bug causing assertion fail on the reaction queue capacity
    when a reaction is triggered my two or more inputs. When two inputs
    trigger the same reaction, the reaction is inserted in the reaction
    queue only once. Hence, the check on the capacity must be done after
    checking whether the reaction is already in the queue.

--- src/queues.c
+++ src/queues.c
@@ -129,14 +129,22 @@ static lf_ret_t ReactionQueue_insert(ReactionQueue* self, Reaction* reaction) {
   validate(reaction);
   validate(reaction->level < (int)self->capacity);
   validate(reaction->level >= 0);
-  validate(self->level_size[reaction->level] < (int)self->capacity);
+  
   validate(self->curr_level <= reaction->level);
 
+  // checking if the reaction to be inserted is already in the queue
+  // e.g., when a reaction is triggered by two or more inputs.
+  // This needs to be done before checking if the queue is full, because
+  // if the reaction is already in the queue, we don't need to insert it again.
   for (int i = 0; i < self->level_size[reaction->level]; i++) {
     if (ACCESS(self->array, self->capacity, reaction->level, i) == reaction) {
       return LF_OK;
     }
   }
+
+  // now we can check if the queue is full
+  validate(self->level_size[reaction->level] < (int)self->capacity);
+
   ACCESS(self->array, self->capacity, reaction->level, self->level_size[reaction->level]) = reaction;
   self->level_size[reaction->level]++;
   if (reaction->level > self->max_active_level) {
