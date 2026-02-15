fd39912ec7b704fe3fdf44a4767f5d62d37c2409
tanneberger <github@tanneberger.me>
2026-02-15 21:36:01 +0100

    optimize generation of multiport connects

--- src/util.c
+++ src/util.c
@@ -9,6 +9,17 @@ void lf_connect(Connection* connection, Port* upstream, Port* downstream) {
   connection->register_downstream(connection, downstream);
 }
 
+void lf_grouped_connect(Connection* connection, Port* upstream, Port* downstream_array, int downstream_size) {
+  for (int i = 0; i < downstream_size; i++) {
+    if (connection->upstream == NULL) {
+      validate(upstream->conns_out_registered < upstream->conns_out_size);
+      upstream->conns_out[upstream->conns_out_registered++] = connection;
+    }
+    connection->upstream = upstream;
+    connection->register_downstream(connection, &downstream_array[i]);
+  }
+}
+
 void lf_connect_federated_output(Connection* connection, Port* output) {
   validate(output->conns_out_registered < output->conns_out_size);
   output->conns_out[output->conns_out_registered++] = connection;
