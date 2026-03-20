d9f6b9702685d5a5e47e0eecdeabc999f8cf1d05
Sebastiano <gaia.franseba@gmail.com>
2026-03-20 10:15:07 +0100

    Refactor FederatedInputConnection_ctor to use explicit if-else for connection type assignment

--- src/federated.c
+++ src/federated.c
@@ -147,7 +147,11 @@ void FederatedInputConnection_ctor(FederatedInputConnection* self, Reactor* pare
                   FederatedInputConnection_prepare, FederatedInputConnection_cleanup, NULL);
   Mutex_ctor(&self->mutex.super);
   self->delay = delay;
-  self->type = is_physical ? PHYSICAL_CONNECTION : LOGICAL_CONNECTION;
+  if (is_physical) {
+    self->type = PHYSICAL_CONNECTION;
+  } else {
+    self->type = LOGICAL_CONNECTION;
+  }
   self->last_known_tag = NEVER_TAG;
   self->max_wait = max_wait;
 }
