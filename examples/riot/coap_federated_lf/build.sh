75f0c9846769d40665118c6880a985e47c841819
tanneberger <github@tanneberger.me>
2026-06-25 11:46:45 +0200

    updating build scripts

--- examples/riot/coap_federated_lf/build.sh
+++ examples/riot/coap_federated_lf/build.sh
@@ -2,7 +2,7 @@
 LF_MAIN=CoapFederatedLF
 
 # Generate configuration templates if they don't exist already
-$REACTOR_UC_PATH/ulf/bin/ulfc-dev --gen-fed-templates src/$LF_MAIN.lf
+$REACTOR_UC_PATH/ulf/bin/ulfc-dev --gen-fed-templates src/$LF_MAIN.ulf
 
 # Generate and build r1 sources
 pushd ./$LF_MAIN/r1
